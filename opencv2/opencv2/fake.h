f5a246a031e78eadba7f1cc478da76f5a49a2fe8
邓江洲 <707703386@qq.com>
2020-09-28 16:41:31 +0800

    #### Open CV 2 , fake

---
+++ opencv2/opencv2/fake.h
@@ -0,0 +1,19 @@
+//
+//  opencv2.h
+//  opencv2
+//
+//  Created by Jz D on 2020/9/28.
+//  Copyright © 2020 Jz D. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for opencv2.
+FOUNDATION_EXPORT double opencv2VersionNumber;
+
+//! Project version string for opencv2.
+FOUNDATION_EXPORT const unsigned char opencv2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <opencv2/PublicHeader.h>
+
+
